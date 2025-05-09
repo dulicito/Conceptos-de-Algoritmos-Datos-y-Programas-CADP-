52cdb845acb022f76b68b065bbdd2c0485a87f42
Dulicito <pv.sofiadulce@gmail.com>
2025-05-09 20:32:33 -0300

    Create ej1p0.pas

---
+++ ⋆˙⟡ practicas/practica 0/ej1p0.pas
@@ -0,0 +1,12 @@
+Program  ej1p0;
+
+Var
+  num1, num2: integer;
+  
+Begin
+  writeln('Ingrese el primer numero:');
+  readln(num1);
+  writeln('Ingrese el segundo numero:');
+  readln(num2);
+  writeln('Se ingresaron los valores ', num2, ' y ', num1);
+End.
