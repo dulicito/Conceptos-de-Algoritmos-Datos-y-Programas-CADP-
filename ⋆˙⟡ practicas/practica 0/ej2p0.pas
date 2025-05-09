f665f58adca45d3a4a52f2625b31959a1c1de5e0
Dulicito <pv.sofiadulce@gmail.com>
2025-05-09 20:35:14 -0300

    Create ej2p0.pas

---
+++ ⋆˙⟡ practicas/practica 0/ej2p0.pas
@@ -0,0 +1,17 @@
+{2. Modifique el programa anterior para que el mensaje de salida muestre la suma de ambos
+números:
+a. Utilizando una variable adicional
+b. Sin utilizar una variable adicional}
+
+Program ej2Ap0;
+Var 
+  num1, num2, suma: integer;
+
+Begin
+  writeln('Ingrese el primer numero:');
+  readln(num1);
+  writeln('Ingrese el segundo numero:');
+  readln(num2);
+  suma := num1 + num2;
+  writeln('La suma de los valores es ', suma);
+End.
